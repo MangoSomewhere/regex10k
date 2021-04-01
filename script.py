7d87f52f803a2228d5747129d40cbebc39613090
drew <andrew_bayola@yahoo.com>
2021-04-01 14:35:52 -0400

    add regex to csv forloop

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 
 import pdfplumber
-
+import csv
 import os
 import re
 
@@ -8,45 +8,39 @@ import re
 
 def extract_whole():
     os.chdir('/Users/drew/Desktop/pythonProject/regex10k/example')
-
     test = os.listdir('/Users/drew/Desktop/pythonProject/regex10k/example')
-
+    csv_fields = ['Company Name']
+    with open('10kcsv.txt', mode='w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(csv_fields)
+        csv_file.close()
     for file in test:
-        with pdfplumber.open(file) as pdf:
-            firstPage = pdf.pages[0]
-            text = firstPage.extract_text()
-            result = text
+        if file.endswith('.pdf'):
+            # open each file as the pdf var
+            with pdfplumber.open(file) as pdf:
+                # stores max number of pages within var
+                all_pages = len(pdf.pages)
+                # loop through the entire document to extract data from pages and store within var result
 
-        companyNameSO = re.compile(r'(\w*) ?(\w*) (CO\.|CORPORATION|INC\.)')
+                result = ''
+                for page in range(all_pages):
+                    data = pdf.pages[page].extract_text()
+                    data.split('\n')
+                    result = result + '\n' + data
+                so_coname = re.compile(r'(\w*) ?(\w*) (CO\.|CORPORATION|INC\.)')
+                mo_coname = so_coname.search(result)
+                row = [mo_coname]
+                rows.append(row)
+                rows = []
+            pdf.close()
 
-        mo = companyNameSO.search(result)
-
-        print(mo)
-        # open each file as the pdf var
-        with pdfplumber.open(file) as pdf:
-            # stores max number of pages within var
-            all_pages = len(pdf.pages)
-            # loop through the entire document to extract data from pages and store within var result
-            result = ''
-            for page in range(all_pages):
-                data = pdf.pages[page].extract_text()
-                data.split('\n')
-                result = result + '\n' + data
-
-    # close pdf to make room for another
-    pdf.close()
+    with open('10kcsv.txt', mode='w') as csv_file:
+        csv_writer.writerow(rows)
+    csv_file.close()
 
 
 extract_whole()
 
-# if page.startswith('Unaudited Consolidated Balance Sheets'):
-'''
-for row in text.split('\n'):
-    if row.startswith('Cash and cash equivalents'):
-        cash2020 = row.split()[-1]
-        cash2019 = row.split()[-2]
-        cash2018 = row.split()[-3]
-'''
 
 
 
